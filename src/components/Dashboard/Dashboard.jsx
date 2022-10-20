a34fd539cf6ca748a6515e2c7afb52cdac95d8b9
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2022-10-20 18:14:27 +0530

    dashboard api completed with year filter

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./dashboard.css";
 import "react-datepicker/dist/react-datepicker.css";
 import AdminNavbar from "../AdminNavbar/AdminNavbar";
@@ -14,10 +14,9 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import { useContext } from "react";
-import { AdminContext } from "../../context/adminContext";
+
 import { useEffect } from "react";
-import { AllAmbulance, AllDoctors, AllPatients, AllPharmas } from "../Axios/apis";
+import { dashboard } from "../Axios/apis";
 
 ChartJS.register(
   CategoryScale,
@@ -33,57 +32,51 @@ const monthsArray = Array.from({ length: 12 }, (item, i) => {
 });
 
 const ClientDashboard = () => {
+ 
+  const [monthValue, setMonthValue] = useState(0);
+  const [yearValue, setYearValue] = useState(2022);
+  const [dashboardData, setDashboardData] = useState(null);
 
-  const {
-    ambulance,
-    setAmbulance,
-    patients,
-    setPatients,
-    doctors,
-    setDoctors,
-    pharma,
-    setPharma,
-  } = useContext(AdminContext);
+  useEffect(() => {
+    const fetchData = async () => {
+      const monthval = parseInt(monthValue) + 1;
+      const date = `01/${monthval < 10 ? `0${monthval}` : `${monthval}`}/${
+        yearValue % 2000
+        }`;
 
-  if (ambulance === null) {
-    const fetchAmbulance = async () => {
-      const res = await AllAmbulance();
-      setAmbulance(res.data.details);
+      const res = await dashboard(date);
+      setDashboardData(res.data);
+
     }
-    fetchAmbulance();
-  }
-  if (doctors === null) {
-    const fetchDoctors = async () => {
-      const res = await AllDoctors();
-      setDoctors(res.data.details);
+    fetchData();
+  }, [monthValue, yearValue])
+
+  var data;
+
+  if (dashboardData !== null) {
+    data = {
+      labels: [...Array(dashboardData?.revArray.length + 1).keys()].slice(1),
+      datasets: [
+        {
+          data: dashboardData?.revArray,
+          backgroundColor: "rgba(8, 143, 143, 0.6)",
+        },
+      ],
     };
-    fetchDoctors();
   }
-  if (patients === null) {
-    const fetchPatients = async () => {
-      const res = await AllPatients();
-      setPatients(res.data.details);
-    }
-    fetchPatients();
-  }
-  if (pharma === null) {
-    const fetchPharma = async () => {
-      const res = await AllPharmas();
-      setPharma(res.data.details);
-    }
-    fetchPharma();
+  else {
+    data = {
+      labels: [...Array(31).keys()].slice(1),
+      datasets: [
+        {
+          data: [...Array(31).keys()].slice(1),
+          backgroundColor: "rgba(8, 143, 143, 0.6)",
+        },
+      ],
+    };
   }
 
-
-  const data = {
-    labels: [...Array(31).keys()].slice(1),
-    datasets: [
-      {
-        data: [...Array(31).keys()].slice(1),
-        backgroundColor: "rgba(8, 143, 143, 0.6)",
-      },
-    ],
-  };
+  
 
   const options = {
   responsive: true,
@@ -123,7 +116,7 @@ const ClientDashboard = () => {
                   <path d="M277.37 11.98C261.08 4.47 243.11 0 224 0c-53.69 0-99.5 33.13-118.51 80h81.19l90.69-68.02zM342.51 80c-7.9-19.47-20.67-36.2-36.49-49.52L239.99 80h102.52zM224 256c70.69 0 128-57.31 128-128 0-5.48-.95-10.7-1.61-16H97.61c-.67 5.3-1.61 10.52-1.61 16 0 70.69 57.31 128 128 128zM80 299.7V512h128.26l-98.45-221.52A132.835 132.835 0 0 0 80 299.7zM0 464c0 26.51 21.49 48 48 48V320.24C18.88 344.89 0 381.26 0 422.4V464zm256-48h-55.38l42.67 96H256c26.47 0 48-21.53 48-48s-21.53-48-48-48zm57.6-128h-16.71c-22.24 10.18-46.88 16-72.89 16s-50.65-5.82-72.89-16h-7.37l42.67 96H256c44.11 0 80 35.89 80 80 0 18.08-6.26 34.59-16.41 48H400c26.51 0 48-21.49 48-48v-41.6c0-74.23-60.17-134.4-134.4-134.4z"></path>
                 </svg>
               </div>
-              <h4>{patients === null ? 0 : patients.length}</h4>
+              <h4>{dashboardData === null ? 0 : dashboardData?.patient}</h4>
               <p>Total Patients</p>
             </Link>
 
@@ -142,7 +135,7 @@ const ClientDashboard = () => {
                   <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zM104 424c0 13.3 10.7 24 24 24s24-10.7 24-24-10.7-24-24-24-24 10.7-24 24zm216-135.4v49c36.5 7.4 64 39.8 64 78.4v41.7c0 7.6-5.4 14.2-12.9 15.7l-32.2 6.4c-4.3.9-8.5-1.9-9.4-6.3l-3.1-15.7c-.9-4.3 1.9-8.6 6.3-9.4l19.3-3.9V416c0-62.8-96-65.1-96 1.9v26.7l19.3 3.9c4.3.9 7.1 5.1 6.3 9.4l-3.1 15.7c-.9 4.3-5.1 7.1-9.4 6.3l-31.2-4.2c-7.9-1.1-13.8-7.8-13.8-15.9V416c0-38.6 27.5-70.9 64-78.4v-45.2c-2.2.7-4.4 1.1-6.6 1.9-18 6.3-37.3 9.8-57.4 9.8s-39.4-3.5-57.4-9.8c-7.4-2.6-14.9-4.2-22.6-5.2v81.6c23.1 6.9 40 28.1 40 53.4 0 30.9-25.1 56-56 56s-56-25.1-56-56c0-25.3 16.9-46.5 40-53.4v-80.4C48.5 301 0 355.8 0 422.4v44.8C0 491.9 20.1 512 44.8 512h358.4c24.7 0 44.8-20.1 44.8-44.8v-44.8c0-72-56.8-130.3-128-133.8z"></path>
                 </svg>
               </div>
-              <h4>{doctors === null ? 0 : doctors.length}</h4>
+              <h4>{dashboardData === null ? 0 : dashboardData?.doctor}</h4>
               <p>Total Doctors</p>
             </Link>
 
@@ -161,7 +154,7 @@ const ClientDashboard = () => {
                   <path d="M217.4 27.43c-27.9.47-53.1 17.11-64.5 42.84l136.5 41.23c6-35.79-15.5-70.49-50.1-81.02-6.2-1.88-12.7-2.91-19.2-3.05h-2.7zm-69.7 60.08c-6.1 35.89 15.4 70.69 50.1 81.19 34.8 10.5 71.9-6.7 86.5-40zm265.5 44.29c-25.3.1-52.2 12.3-72.5 41L215.9 349.7c-33.5 47.4-18.9 97 14.1 120.4 33.1 23.5 84.6 20.8 118.1-26.6l124.7-176.8c33.5-47.5 18.9-97-14.1-120.5-12.4-8.8-27.3-13.9-43-14.4zm-1.8 17.3c1.3 0 2.6 0 3.8.1 12.1.5 23.5 4.8 33.1 11.7 25.7 18.2 38.6 54.5 9.7 95.4l-64.5 91.5c-35.8-9.6-81.8-42.3-102.7-73l64.7-91.6c16.9-23.9 37-33.7 55.9-34.1zM91.25 225.3c-9.62.1-19.11 2.1-27.93 6-33.11 14.5-50.34 51.5-40.24 86.3l130.72-57.1c-13.1-22.1-36.9-35.5-62.55-35.2zm69.65 51.6L30.2 334.1c18.45 31.4 57.3 44 90.6 29.5 33.2-14.6 50.4-51.8 40.1-86.7z"></path>
                 </svg>
               </div>
-              <h4>{pharma === null ? 0 : pharma.length}</h4>
+              <h4>{dashboardData === null ? 0 : dashboardData?.pharmacy}</h4>
               <p>Total Pharmas</p>
             </Link>
 
@@ -180,7 +173,7 @@ const ClientDashboard = () => {
                   <path d="M624 352h-16V243.9c0-12.7-5.1-24.9-14.1-33.9L494 110.1c-9-9-21.2-14.1-33.9-14.1H416V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h16c0 53 43 96 96 96s96-43 96-96h128c0 53 43 96 96 96s96-43 96-96h48c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM160 464c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm144-248c0 4.4-3.6 8-8 8h-56v56c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8v-56h-56c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h56v-56c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v56h56c4.4 0 8 3.6 8 8v48zm176 248c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm80-208H416V144h44.1l99.9 99.9V256z"></path>
                 </svg>
               </div>
-              <h4>{ambulance === null ? 0 : ambulance.length}</h4>
+              <h4>{dashboardData === null ? 0 : dashboardData?.ambulance}</h4>
               <p>Total Ambulance</p>
             </Link>
 
@@ -199,20 +192,32 @@ const ClientDashboard = () => {
                   <path d="M258 21.89c-.5 0-1.2 0-1.8.12-4.6.85-10.1 5.1-13.7 14.81-3.8 9.7-4.6 23.53-1.3 38.34 3.4 14.63 10.4 27.24 18.2 34.94 7.6 7.7 14.5 9.8 19.1 9 4.8-.7 10.1-5.1 13.7-14.7 3.8-9.64 4.8-23.66 1.4-38.35-3.5-14.8-10.4-27.29-18.2-34.94-6.6-6.8-12.7-9.22-17.4-9.22zM373.4 151.4c-11 .3-24.9 3.2-38.4 8.9-15.6 6.8-27.6 15.9-34.2 24.5-6.6 8.3-7.2 14.6-5.1 18.3 2.2 3.7 8.3 7.2 20 7.7 11.7.7 27.5-2.2 43-8.8 15.5-6.7 27.7-15.9 34.3-24.3 6.6-8.3 7.1-14.8 5-18.5-2.1-3.8-8.3-7.1-20-7.5-1.6-.3-3-.3-4.6-.3zm-136.3 92.9c-6.6.1-12.6.9-18 2.3-11.8 3-18.6 8.4-20.8 14.9-2.5 6.5 0 14.3 7.8 22.7 8.2 8.2 21.7 16.1 38.5 20.5 16.7 4.4 32.8 4.3 44.8 1.1 12.1-3.1 18.9-8.6 21.1-15 2.3-6.5 0-14.2-8.1-22.7-7.9-8.2-21.4-16.1-38.2-20.4-9.5-2.5-18.8-3.5-27.1-3.4zm160.7 58.1L336 331.7c4.2.2 14.7.5 14.7.5l6.6 8.7 54.7-28.5-14.2-10zm-54.5.1l-57.4 27.2c5.5.3 18.5.5 23.7.8l49.8-23.6-16.1-4.4zm92.6 10.8l-70.5 37.4 14.5 18.7 74.5-44.6-18.5-11.5zm-278.8 9.1a40.33 40.33 0 0 0-9 1c-71.5 16.5-113.7 17.9-126.2 17.9H18v107.5s11.6-1.7 30.9-1.8c37.3 0 103 6.4 167 43.8 3.4 2.1 10.7 2.9 19.8 2.9 24.3 0 61.2-5.8 69.7-9C391 452.6 494 364.5 494 364.5l-32.5-28.4s-79.8 50.9-89.9 55.8c-91.1 44.7-164.9 16.8-164.9 16.8s119.9 3 158.4-27.3l-22.6-34s-82.8-2.3-112.3-6.2c-15.4-2-48.7-18.8-73.1-18.8z"></path>
                 </svg>
               </div>
-              <h4>18900</h4>
+              <h4>{dashboardData === null ? 0 : dashboardData?.totalRevenue}</h4>
               <p>Total Revnue</p>
             </Link>
           </div>
 
           <div className="revenue-container">
             <h1 className="heading">Revenue</h1>
-            <select>
-              {monthsArray.map((month, i) => (
-                <option key={i} value={month}>
-                  {month}
-                </option>
-              ))}
-            </select>
+            <div>
+              <select
+                value={monthValue}
+                onChange={(e) => setMonthValue(e.target.value)}
+              >
+                {monthsArray.map((month, i) => (
+                  <option key={i} value={i}>
+                    {month}
+                  </option>
+                ))}
+              </select>
+              <select
+                value={yearValue}
+                onChange={(e) => setYearValue(e.target.value)}
+              >
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+              </select>
+            </div>
           </div>
 
           <div className="bar-graph-container">
